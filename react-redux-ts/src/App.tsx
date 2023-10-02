c0fe79402dcffd324bc7477fb58ca5f573328cc0
Ananth <ananthsoft@gmail.com>
2023-10-02 01:47:59 -0700

    Redux functionaly implemented to UI

--- react-redux-ts/src/App.tsx
+++ react-redux-ts/src/App.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators, State } from "./state";
 
 function App() {
+  const dispatch = useDispatch();
+  const { depositMoney, withdraw, bankrupt } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+  const amount = useSelector((state: State) => state.bank);
+
   return (
     <div className="App">
-      <h1>0</h1>
-      <button className="btn btn-primary">Deposit</button>
-      <button className="btn btn-primary">Withdraw</button>
-      <button className="btn btn-primary">Bankrupt</button>
+      <h1>{amount}</h1>
+      <button className="btn btn-primary" onClick={() => depositMoney(1000)}>
+        Deposit
+      </button>
+      <button className="btn btn-primary" onClick={() => withdraw(10)}>
+        Withdraw
+      </button>
+      <button className="btn btn-primary" onClick={() => bankrupt()}>
+        Bankrupt
+      </button>
     </div>
   );
 }
